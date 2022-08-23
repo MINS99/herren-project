a198cb07afcee7b4bf12c4bbd5d4f7ad70ea7ab4
Celina <luv.mins99@gmail.com>
2022-08-23 12:08:03 +0900

    feat: 샵 정보 추가

---
+++ src/main/java/com/manage/shop/shop/domain/Shop.java
@@ -0,0 +1,49 @@
+package com.manage.shop.shop.domain;
+
+import com.manage.shop.staff.domain.Staff;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Shop {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String shopName;
+    @Column(nullable = false, unique = true)
+    private String bizNumber;
+    @Column(nullable = false)
+    private String phoneNumber;
+    private String kakaoId;
+    @OneToMany(mappedBy = "shop")
+    private List<Staff> staffList;
+
+    protected Shop() {
+    }
+
+    public String getShopName() {
+        return shopName;
+    }
+
+    public String getBizNumber() {
+        return bizNumber;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getKakaoId() {
+        return kakaoId;
+    }
+
+    public List<Staff> getStaffList() {
+        return staffList;
+    }
+}
