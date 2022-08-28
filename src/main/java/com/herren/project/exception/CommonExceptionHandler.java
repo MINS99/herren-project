9d86f0b09d7d3a752e28441b12e6d96193fd0bc0
Celina <luv.mins99@gmail.com>
2022-08-28 19:49:23 +0900

    feat: 요청 객체 유효성 검증 및 예외 처리 로직 추가

--- src/main/java/com/herren/project/exception/CommonExceptionHandler.java
+++ src/main/java/com/herren/project/exception/CommonExceptionHandler.java
@@ -2,7 +2,9 @@ package com.herren.project.exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,5 +16,12 @@ public class CommonExceptionHandler {
     protected ResponseEntity<ErrorResponse> handleCustomException(CommonException e) {
         logger.error("{} : {}", e.getErrorCode(), e.getErrorCode().getDetail());
         return ErrorResponse.toResponseEntity(e.getErrorCode());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> methodArgumentValidException(MethodArgumentNotValidException e) {
+        String message = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+        logger.error("invalid param : {}", message);
+        return ErrorResponse.toResponseEntity(HttpStatus.BAD_REQUEST, message);
     }
 }
